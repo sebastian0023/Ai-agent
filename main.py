e8a2cb69a165c85d427441e218322496be4bed05
sebastian0023 <130592260+sebastian0023@users.noreply.github.com>
2025-10-18 15:56:22 -0600

    Implement AI assistant with really simple calculation capability

---
+++ main.py
@@ -0,0 +1,44 @@
+from langchain_core.messages import HumanMessage
+from langchain_openai import ChatOpenAI
+from langchain.tools import tool
+from langchain.agents import create_agent
+from dotenv import load_dotenv
+
+load_dotenv()
+
+@tool
+def add_numbers(a, b):
+    return a + b
+
+def main():
+    model = ChatOpenAI(temperature=0)
+    tools = [add_numbers]
+    agent_executor = create_agent(model, tools)
+
+    print("I am your AI assistant. Type q to exit.")
+    print("Now I can perform simple calculations or chat with you.")
+
+    Flag = True
+
+    while Flag:
+        user_input = input("\nYou: ").strip()
+
+        if user_input.lower() == "q":
+            Flag = False
+            break
+        
+        print("\nAssistant: ", end="")
+        for chunk in agent_executor.stream({"messages": [HumanMessage(content=user_input)]}):
+            if "agent" in chunk and "messages" in chunk["agent"]:
+                for message in chunk["agent"]["messages"]:
+                    print(message.content, end="")
+
+        print()
+
+if __name__ == "__main__":
+    main()
+
+
+
+ 
+
